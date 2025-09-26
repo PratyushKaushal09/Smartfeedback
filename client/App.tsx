57f8b171a3fb5f20648b88f4b025a3d3edb90826
Pratyush Kaushal <pratyushkaushal09@users.noreply.github.com>
2025-09-26 06:35:17 +0000

    perf: preconnect Google Fonts, theme-color per mode, preload hero; MotionConfig respects reduced motion

--- client/App.tsx
+++ client/App.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MotionConfig } from "framer-motion";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Quiz from "./pages/Quiz";
@@ -13,23 +14,25 @@ import BuilderProvider from "./builder/BuilderProvider";
 import BuilderPage from "./pages/BuilderPage";
 
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <BuilderProvider>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/quiz/:sport" element={<Quiz />} />
-            <Route path="/b/*" element={<BuilderPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BuilderProvider>
-    </BrowserRouter>
-  </TooltipProvider>
+  <MotionConfig reducedMotion="user">
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <BuilderProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/quiz/:sport" element={<Quiz />} />
+              <Route path="/b/*" element={<BuilderPage />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BuilderProvider>
+      </BrowserRouter>
+    </TooltipProvider>
+  </MotionConfig>
 );
 
 createRoot(document.getElementById("root")!).render(<App />);
